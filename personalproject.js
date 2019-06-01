0fb91cb664058255d9befa36c30f6cfe07861c8c
Yasmin Abdulhadi <yasmin@MacBook-Pro.attlocal.net>
2019-06-01 11:40:45 -0500

    update

---
+++ personalproject.js
@@ -0,0 +1,11 @@
+var cardContainer = $('.cardContainer');
+var submit = $('.button');
+submit.on('click', clicksubmit)
+  function clicksubmit(event) {
+    event.preventDefault()
+    debugger
+    console.log("apples");
+  }
+
+//function submit_by_class() {
+//var cardContainer = document.getElementByClassName ("cardContainer").value;
